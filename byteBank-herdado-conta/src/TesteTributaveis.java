94e99f9a0dca5e93840d256e5b30a257718525bd
Carla Dias <carllottadias@gmail.com>
2020-10-01 18:23:40 -0300

    Finalizando com a classe de teste

---
+++ byteBank-herdado-conta/src/TesteTributaveis.java
@@ -0,0 +1,19 @@
+
+public class TesteTributaveis {
+
+	public static void main(String[] args) {
+
+		ContaCorrente cc = new ContaCorrente(222, 555);
+		cc.deposita(100.0);
+		
+		SeguroDeVida sv = new SeguroDeVida();
+		
+		CalculadorDeImposto calc = new CalculadorDeImposto();
+		calc.registra(cc);
+		calc.registra(sv);
+		
+		
+		System.out.println(calc.getTotalImposto());
+	}
+
+}
