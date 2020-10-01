4b91333a6177e608010b77646d8a33128a3dc5ca
Carla Dias <carllottadias@gmail.com>
2020-10-01 16:23:58 -0300

    Passando a autenticar um gerente implementando a interface de autenticação

--- byteBank-herdado/src/Gerente.java
+++ byteBank-herdado/src/Gerente.java
@@ -1,10 +1,25 @@
-public class Gerente extends FuncionarioAutenticavel {
-	 
+//Gerente eh um Funcionario, Gerente herda da class Funcionario, assina o contrato Autenticavel, eh um Autenticavel
+public class Gerente extends Funcionario implements Autenticavel{
+	
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
 	public double getBonificacao() {
-		
-		System.out.println("Método específico do gerente, executado!");
+		System.out.println("Chamando o método de bonificacao do GERENTE");
 		return super.getSalario();
-}
-	 
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
+	}
 
 }
